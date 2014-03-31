300f17c8682d364d31e046dea34cdaa36bc951a0
Modsky1 <maude.jake@gmail.com>
2014-03-31 10:53:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birds2::Application.config.secret_key_base = '4e18f7dfa4bcd672bd0f2e55789af0e9282711af239161b412316bbfe9b4fd258bb1c23d45a2230eaf39fa417148e070106861879fa85148dea62ef25c3751ce'
